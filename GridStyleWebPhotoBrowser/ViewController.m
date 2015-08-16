0e8fa933447fa36bd2568f55edd0816ca7ae736b
howeguo <guo@tataufo.com>
2015-08-16 17:26:28 +0800

    add screen image and modify gridView  bgColor

--- GridStyleWebPhotoBrowser/ViewController.m
+++ GridStyleWebPhotoBrowser/ViewController.m
@@ -22,7 +22,7 @@
     NSArray *largeImgURLs = @[[NSURL URLWithString:@"http://fe.topit.me/e/ea/d2/113421799164ad2eael.jpg"],[NSURL URLWithString:@"http://i4.topit.me/4/de/13/1134214094cf213de4l.jpg"],[NSURL URLWithString:@"http://img5.duitang.com/uploads/item/201501/09/20150109233247_NsCGY.jpeg"],[NSURL URLWithString:@"http://img.tvmao.com/stills/star/3/202/b/580x441_1.jpg"],[NSURL URLWithString:@"http://img4.duitang.com/uploads/item/201412/24/20141224193945_xjJ3y.jpeg"],[NSURL URLWithString:@"http://img5.duitang.com/uploads/item/201501/09/20150109233247_NsCGY.jpeg"],[NSURL URLWithString:@"http://img.tvmao.com/stills/star/3/202/b/580x441_1.jpg"],[NSURL URLWithString:@"http://img4.duitang.com/uploads/item/201412/24/20141224193945_xjJ3y.jpeg"],[NSURL URLWithString:@"http://img4.duitang.com/uploads/item/201412/24/20141224193945_xjJ3y.jpeg"]];
     
     PhotoGridViewBrowser *photosView = [[PhotoGridViewBrowser alloc] initWithFrame:CGRectMake(40.0, 30.0, 290.0f, 90.0f)];
-    photosView.backgroundColor = [UIColor redColor];
+    photosView.backgroundColor = [UIColor purpleColor];
     photosView.imgSize = 90.0f;
     photosView.distance = 5.0f;
     photosView.columnsPreRow = 3;
@@ -37,7 +37,7 @@
     NSArray *largeImgURLs1 = @[[NSURL URLWithString:@"http://fe.topit.me/e/ea/d2/113421799164ad2eael.jpg"],[NSURL URLWithString:@"http://i4.topit.me/4/de/13/1134214094cf213de4l.jpg"],[NSURL URLWithString:@"http://img5.duitang.com/uploads/item/201501/09/20150109233247_NsCGY.jpeg"],[NSURL URLWithString:@"http://img.tvmao.com/stills/star/3/202/b/580x441_1.jpg"],[NSURL URLWithString:@"http://img4.duitang.com/uploads/item/201412/24/20141224193945_xjJ3y.jpeg"],[NSURL URLWithString:@"http://img5.duitang.com/uploads/item/201501/09/20150109233247_NsCGY.jpeg"],[NSURL URLWithString:@"http://img.tvmao.com/stills/star/3/202/b/580x441_1.jpg"],[NSURL URLWithString:@"http://img4.duitang.com/uploads/item/201412/24/20141224193945_xjJ3y.jpeg"]];
     
     PhotoGridViewBrowser *photosView1 = [[PhotoGridViewBrowser alloc] initWithFrame:CGRectMake(40.0, 330.0, 305.0f, 90.0f)];
-    photosView1.backgroundColor = [UIColor redColor];
+    photosView1.backgroundColor = [UIColor yellowColor];
     photosView1.imgSize = 70.0f;
     photosView1.distance = 5.0f;
     photosView1.columnsPreRow = 4;
